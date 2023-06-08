d61de4b98bac795bf67248fdb3cadde324cdf8cc
MaxBarulin <107652343+MaxBarulin@users.noreply.github.com>
2023-06-08 19:32:50 +0300

    Update head.js

--- js/head.js
+++ js/head.js
@@ -43,7 +43,7 @@ function init() {
     const loader = new THREE.GLTFLoader();
     loader.load('./models/WS.gltf', gltf => {
         mesh = gltf.scene.children[0];
-        mesh.scale.set(.001, .001, .001);
+        mesh.scale.set(.01, .01, .01);
         /*mesh.position.x(0)*/
         scene.add(mesh);
 
