eb40ca99d1d171fe30db88547334463002a8f1ca
MaxBarulin <107652343+MaxBarulin@users.noreply.github.com>
2023-12-07 00:17:53 +0300

    Update scenehorse.js

--- js/scenehorse.js
+++ js/scenehorse.js
@@ -41,9 +41,10 @@ function init() {
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     const loader = new THREE.GLTFLoader();
-    loader.load('./models/Horse.glb', gltf => {
+    loader.load('./models/WS1.gltf', gltf => {
         mesh = gltf.scene.children[0];
-        mesh.scale.set(0.01, 0.01, 0.01);
+        mesh.scale.set(0.05, 0.05, 0.05);
+        mesh.position.set(0, -1, 1);
         scene.add(mesh);
 
         mixer = new THREE.AnimationMixer(mesh);
