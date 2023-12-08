6176c60ba6c5e9178e1c14a06398e6e26d1c2d97
MaxBarulin <107652343+MaxBarulin@users.noreply.github.com>
2023-12-08 14:05:34 +0300

    Update scenehorse.js

--- js/scenehorse.js
+++ js/scenehorse.js
@@ -43,7 +43,7 @@ function init() {
     const loader = new THREE.GLTFLoader();
     loader.load('./models/satanhands.gltf', gltf => {
         mesh = gltf.scene.children[0];
-        mesh.scale.set(0.01, 0.01, 0.01);
+        mesh.scale.set(1, 1, 1);
         mesh.position.set(-2.7, 0, 0);
         mesh.rotation.set(0, 0, 0);
         scene.add(mesh);
