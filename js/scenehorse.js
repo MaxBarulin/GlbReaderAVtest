61e3e59ebd8d5d61ae18028aa35fd87c4bafc39a
MaxBarulin <107652343+MaxBarulin@users.noreply.github.com>
2023-12-15 22:37:20 +0300

    Update scenehorse.js

--- js/scenehorse.js
+++ js/scenehorse.js
@@ -42,8 +42,8 @@ function init() {
 
     const loader = new THREE.GLTFLoader();
     /*loader.load('./models/clocker11.gltf', gltf => {*/
-    /*loader.load('./models/clocker11.glb', gltf => {*/
-    loader.load('./models/satanhands.gltf', gltf => {
+    loader.load('./models/clocker1111.gltf', gltf => {
+    /*loader.load('./models/satanhands.gltf', gltf => {*/
         mesh = gltf.scene.children[0];
         mesh.scale.set(14, 14, 14);
         mesh.position.set(0, -18, -4);
